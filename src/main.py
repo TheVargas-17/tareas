676207fd6c197bc01140686291489eca7888883e
TheVargas-17 <23308060610434@cetis61.edu.mx>
2026-04-14 07:29:18 -0500

    update

--- src/main.py
+++ src/main.py
@@ -1,6 +1,34 @@
+import flet as ft
+from controllers.userController import AuthController
+from controllers.TareaController import TareaController
+from views.login import LoginView
+from views.dashboard import DashboardView
+
+def start(page: ft.Page):
+
+    def route_change(e):
+        page.views.clear()
+
+        # Caso de seguridad: Si algo falla, mostrar texto de error
+        if not page.views:
+            page.views.append(
+                ft.View(
+                    "/",
+                    [ft.Text("Error: Ruta no encontrada o vista vacía")]
+                )
+            )
+
+        page.update()
+
+    page.on_route_change = route_change
+
+    # Forzamos la navegación inicial
+    page.go("/")
+
+
 def main():
-    print("Hello from tareas!")
-    
+    # Ejecución de la app
+    ft.app(target=start)
 
 
 if __name__ == "__main__":
